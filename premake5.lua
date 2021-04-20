7527f6d50c338c194303b19f285aaebc82e4d94f
- The Aarnold - <mane.imation9@gmail.com>
2021-04-20 23:14:12 +0200

    Initial commit

---
+++ premake5.lua
@@ -0,0 +1,69 @@
+workspace "ZucchiniTest"
+   architecture "x86_64"
+   configurations { "Debug", "Release" }
+
+project "TestField"
+   kind "ConsoleApp"
+   language "C++"
+   cppdialect "C++17"
+
+   targetdir("%{prj.location}/bin/%{cfg.buildcfg}/%{prj.name}")
+   objdir("%{prj.location}/bin/%{cfg.buildcfg}/out")
+
+   files {
+      "src/**.cpp",
+      "include/**.hpp",
+      "include/**.inl"
+   }
+
+   includedirs {
+      "include",
+      "%{wks.location}/ZucchiniEngine/include"
+   }
+
+   libdirs {
+      "%{wks.location}/ZucchiniEngine/lib"
+   }
+
+   filter "system:windows"
+      systemversion "latest"
+
+   filter "system:linux"
+      includedirs {
+         "/usr/include",
+         "/usr/local/include"
+      }
+      libdirs {
+         "/usr/lib",
+         "/usr/local/lib"
+      }
+      links {
+         "dl",
+         "pthread"
+      }
+
+   filter { "action:gmake*", "toolset:gcc" }
+      buildoptions {
+         "-Wall", "-Wextra", "-Wold-style-cast", "-Woverloaded-virtual", "-Wfloat-equal", "-Wwrite-strings",
+         "-Wpointer-arith", "-Wcast-qual", "-Wcast-align", "-Wconversion", "-Wshadow", "-Wredundant-decls",
+         "-Wdouble-promotion", "-Winit-self", "-Wswitch-default", "-Wswitch-enum", "-Wundef", "-Wlogical-op", "-Winline",
+         "-m64", "-fexceptions", "-pedantic"
+      }
+      linkoptions {
+         "-m64"
+      }
+
+   filter "configurations:Debug"
+      links {
+         "zengine-d"
+      }
+      runtime "Debug"
+      symbols "On"
+      
+
+   filter "configurations:Release"
+      links {
+         "zengine"
+      }
+      runtime "Release"
+      optimize "On"
