57679f8a01ed91959acd24becf0dce4cbd014ddb
- The Aarnold - <mane.imation9@gmail.com>
2021-04-22 17:39:20 +0200

    Update workspace premake

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,16 @@
 workspace "ZucchiniTest"
    architecture "x86_64"
    configurations { "Debug", "Release" }
+   
+   includedirs {
+      "ZucchiniEngine/ZEngine/include",
+      "ZGraphics2D/ZGraphics2D/include"
+   }
+   
+   libdirs {
+      "ZucchiniEngine/ZEngine/lib",
+      "ZGraphics2D/ZGraphics2D/lib"
+   }
 
 include "TestField"
 include "ZucchiniEngine/ZEngine"
